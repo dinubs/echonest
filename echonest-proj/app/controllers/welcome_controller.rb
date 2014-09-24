7cbcb3a5ecce3827b2ef45a21c93c285aceec8a7
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-23 18:54:58 -0700

    Different search, if songs are empty then return

--- echonest-proj/app/controllers/welcome_controller.rb
+++ echonest-proj/app/controllers/welcome_controller.rb
@@ -2,14 +2,19 @@ class WelcomeController < ApplicationController
   def index
   	song = Echonest::Song.new('K1RQLGXWBMUAYZOAI')
 
-	params = { song_min_hotttnesss: 0.8, format: "json"}
+	params = { song_min_hotttnesss: 0.5, mood: "happy", format: "json"}
 	@Songs = song.search(params)
 
 	# create a client object with your app credentials
 	@client = Soundcloud.new(:client_id => 'ab607d302c6da74db4b68567374ba72c')
 
 	# find all sounds of buskers licensed under 'creative commons share alike'
-	@track = @client.get('/tracks', :q => @Songs[0][:title], :limit => 1)[0]
+	if @Songs.empty?
+		@search = false
+	else
+		@search = true
+		@track = @client.get('/tracks', :q => @Songs[0][:title], :limit => 1)
+	end
 	# @Tracks = client.get('/oembed', track)
   end
 end
