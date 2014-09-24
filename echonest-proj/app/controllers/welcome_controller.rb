ed5b80ca43c01a7f08029aad634dac665003ba0f
gavindinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-09-23 21:34:31 -0700

    Better Echonest Wrapper

--- echonest-proj/app/controllers/welcome_controller.rb
+++ echonest-proj/app/controllers/welcome_controller.rb
@@ -1,20 +1,22 @@
 class WelcomeController < ApplicationController
+  Echowrap.configure do |config|
+  	config.api_key =       'K1RQLGXWBMUAYZOAI'
+  	config.consumer_key =  '2ec1d1afba154f4adc036cb494ef6cb3'
+  	config.shared_secret = 'fBkzP7rBRL+LnTyw5y3zdQ'
+  end
+  
   def index
-  	song = Echonest::Song.new('K1RQLGXWBMUAYZOAI')
 
-	params = { song_min_hotttnesss: 0.5, mood: "happy", format: "json"}
-	@Songs = song.search(params)
+  	@Songs =  Echowrap.song_search(artist: 'Childish Gambino')
 
 	# create a client object with your app credentials
 	@client = Soundcloud.new(:client_id => 'ab607d302c6da74db4b68567374ba72c')
 
-	# find all sounds of buskers licensed under 'creative commons share alike'
 	if @Songs.empty?
 		@search = false
 	else
 		@search = true
-		@track = @client.get('/tracks', :q => @Songs[0][:title], :limit => 1)
+		@track = @client.get('/tracks', :q => @Songs[3].title, :limit => 10)
 	end
-	# @Tracks = client.get('/oembed', track)
   end
 end
